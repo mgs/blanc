1484b8f3ede2d828bc047b49625524f235d2b66f
Michael Simpson <mgsimpson@gmail.com>
2012-04-26 03:52:34 -0400

    new project.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject blanc "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "1.0.2"]
+                 [markdown-clj "0.6"]])

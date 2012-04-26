30ed2e111f29fbccecfcbf135fcfcc92a574b4a9
Michael Simpson <mgsimpson@gmail.com>
2012-04-26 03:50:42 -0400

    new repo

---
+++ resources/2013/2/28/example-file.md
@@ -0,0 +1,3 @@
+#This is just an example.
+
+##Fancy title.
---
+++ test/blanc/test/core.clj
@@ -0,0 +1,6 @@
+(ns blanc.test.core
+  (:use [blanc.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

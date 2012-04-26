c854b2180bbc33788ad83bf3f69999332f085323
Michael Simpson <mgsimpson@gmail.com>
2012-04-26 04:07:44 -0400

    On to the 1.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject blanc "1.0.0-SNAPSHOT"
   :description "This is a minimalist's web framework for developing file-system based websites using Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.0.2"]
                  [markdown-clj "0.6"]])
